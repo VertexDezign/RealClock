699f3635b62bd038ab0b9b60890cfdb695eea247
Benjamin Leber <grisu118@grisu118.ch>
2017-09-11 20:58:59 +0200

    + Add support for uiScale

--- RealClock.lua
+++ RealClock.lua
@@ -2,11 +2,13 @@
 --
 -- @author  Grisu118 - VertexDezign.net
 -- @history		v1.0 	- 2016-10-25 - Initial implementation
+--              v1.1    - 2017-09-11 - Add use of UI Scale
 -- @Descripion: Shows the real time clock in the upper right corner
 -- @web: http://grisu118.ch or http://vertexdezign.net
 -- Copyright (C) Grisu118, All Rights Reserved.
 
 RealClock = {}
+RealClock.fontSize = 0.02
 
 function RealClock:loadMap(name)
 end
@@ -21,7 +23,11 @@ end
 
 function RealClock:draw()
     setTextColor(1,1,1,1)
-    renderText(0.94, 0.98, 0.02, getDate("%H:%M:%S"))
+    local fontSize = g_gameSettings:getValue("uiScale") * RealClock.fontSize
+    local date = getDate("%H:%M:%S")
+    local width = getTextWidth(fontSize, date)
+    local height = getTextHeight(fontSize, date)
+    renderText(0.99 - width, 1 - height, fontSize, date)
     setTextColor(1,1,1,0)
 end
 
